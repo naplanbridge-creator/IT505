d030ea3009adf5011efc193f8872889c81a6d8ae
tr-ahmed <tr.ahmedh@gmail.com>
2026-05-03 21:37:06 +0300

    Rename app to HR System

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -6,9 +6,9 @@ import { DocumentationPageComponent } from './pages/documentation-page.component
 import { HomePageComponent } from './pages/home-page.component';
 
 export const routes: Routes = [
-	{ path: '', component: HomePageComponent, title: 'IT505 | Home' },
-	{ path: 'departments', component: DepartmentPageComponent, title: 'IT505 | Departments' },
-	{ path: 'employees', component: EmployeePageComponent, title: 'IT505 | Employees' },
-	{ path: 'documentation', component: DocumentationPageComponent, title: 'IT505 | Documentation' },
+	{ path: '', component: HomePageComponent, title: 'HR System | Home' },
+	{ path: 'departments', component: DepartmentPageComponent, title: 'HR System | Departments' },
+	{ path: 'employees', component: EmployeePageComponent, title: 'HR System | Employees' },
+	{ path: 'documentation', component: DocumentationPageComponent, title: 'HR System | Documentation' },
 	{ path: '**', redirectTo: '' }
 ];
